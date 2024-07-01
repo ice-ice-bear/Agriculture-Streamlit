ea1241b6a092ad896c7839a974cf0b81b36e1bf2
re2panda <3balbi7@gmail.com>
2024-07-01 17:53:34 +0900

    Change title

--- app.py
+++ app.py
@@ -222,7 +222,8 @@ def plot_risk_area_grades(df):
     
     fig, ax = plt.subplots(figsize=(12, 6))
     pivot_table.plot(kind='bar', ax=ax)
-    ax.set_title('재해위험지구 유형에 따른 재해 등급')
+    # ax.set_title('재해위험지구 유형에 따른 재해 등급')
+    st.title("재해위험지구 유형에 따른 재해 등급")
     ax.set_xlabel('DST_RSK_DSTRCT_GRD_CD')
     ax.set_ylabel('Count')
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
